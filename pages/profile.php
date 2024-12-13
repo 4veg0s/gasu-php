ede466d2316a0b39d8a7998968c8e67cbed6e408
4veg0s <tkachyov03@inbox.ru>
2024-12-13 18:29:24 +0300

    halfway through reports

--- pages/profile.php
+++ pages/profile.php
@@ -48,11 +48,142 @@ echo '<div class="profile-container">' .
             '<p><strong>Логин:</strong> ' . $user['login'] . '</p>' .
             '<p><strong>Город:</strong> ' . $user['name'] . '</p>' .
             create_link_button(chat_pointer($user['user_id']), 'Перейти в диалог') .
+            '<button class="common-button" onclick="toggleReports()">Комментарии о профиле</button>' .
             create_link_button($welcome_page_path, 'На главную') .
         '</div>' .
     '</div>';
-// отображение аватарки фио и тд
-// кнопка для перехода в диалог из профиля
-// кнопка перехода на всех пользователей
 
+$id_from = $_SESSION['id_user'];
+if ($id_from != '') {
+    // echo '<pre>';
+    // echo print_r($_GET);
+    // echo '</pre>';
+
+    $id_to = $user_id;
+
+    $sql_user_to = 'select * from user where user.id = ' . $id_to;
+    $sql_user_from = 'select * from user where user.id = ' . $id_from;
+
+    $user_to = $conn->query($sql_user_to)->fetch_assoc();
+    $user_from = $conn->query($sql_user_from)->fetch_assoc();
+
+    $sql_delete_message_by_id = '';
+    if (isset($_POST['deleteMessageSubmit'])) {
+        $message_id_to_delete = $_POST['hidden-delete-id'];
+        $sql_delete_message_by_id = 'DELETE FROM `report` where `id` = ' . $message_id_to_delete;
+
+        $conn->query($sql_delete_message_by_id);
+
+        header("Location: " . $_SERVER['PHP_SELF'] . '?id_to=' . $id_to);
+        // exit();
+    }
+    if (isset($_POST['sendMessage'])) {
+        if ($_POST['report'] != '') {
+            $str = 'INSERT INTO report (
+                `id_from`, `id_to`, `text`, `creation`, `status`
+            ) VALUES (' . 
+            '' . $user_from['id'] . ',' .
+            '' . $user_to['id'] . ',' .
+            '"' . $_POST['report'] . '",' .
+            'NOW(),' .
+            '' . 1 . '' .
+            ')';
+            
+            $conn->query($str);
+
+            header("Location: " . $_SERVER['PHP_SELF'] . '?id_to=' . $id_to);
+            // exit();
+        }
+    }
+
+    $sql_select_all_reports_by_to = 'SELECT user.F, user.I, user.O, profile_report.id, profile_report.id_from, profile_report.id_to, profile_report.text, profile_report.creation 
+    FROM `profile_report` JOIN `user` 
+    ON user.id = ' . $id_to . ' and profile_report.id_to = ' . $id_to;
+    
+    $sql_reports_ordered = '(' . $sql_select_all_reports_by_to . ') order by creation';
+    
+    $all_reports_ordered_by_creation = $conn->query($sql_reports_ordered);
+    
+    $all_reports_ordered_by_creation_array = array();
+
+    if ($all_reports_ordered_by_creation->num_rows > 0) {
+        while ($report = $all_reports_ordered_by_creation->fetch_assoc()) {
+            $all_reports_ordered_by_creation_array[] = $report;
+        }
+    }
+
+    echo '<div class="reports-container" id="reports">';
+
+    echo '<h2><center>Отзывы о пользователе ' . $user_to['F'] . ' ' . $user_to['I'] . ' ' . $user_to['O'] . '</center></h2>';
+    
+    $files = scandir($file_destination_path);
+    $report_blocks = '';
+    $next_date = '';
+    foreach ($all_reports_ordered_by_creation_array as $report) {
+        $current_user = $report['id_from'] == $user_from['id'] ? $user_from : $user_to;
+        $img_file = '';
+        foreach ($files as $file) {
+            if (strpos($file, $current_user['login']) !== false) {
+                $img_file = $file;
+            }
+        }
+        if ($img_file == '') {
+            $img_file = $placeholder_image_name;
+        }
+        $img_route = $appserv_route_dir . '/' . $img_file;
+
+        $prev_date = $next_date;
+        $report_block = '';
+        $next_date = explode(' ', $report['creation'])[0];
+        
+        // $message_time = explode(' ', $report['creation'])[1];
+
+        $report_author_avatar = profile_link($current_user['id'], '<img src="' . $img_route . '" class="" title="' . $current_user['F'] . ' ' . $current_user['I'] . ' ' . $current_user['O'] . '"/>');
+
+        $next_report = '<div class="report">
+            <div class="review-header">
+                    <div class="report-author">' .
+                        $report_author_avatar .
+                        '<span>' . $current_user['F'] . ' ' . $current_user['I'] . ' ' . $current_user['O'] . '</span>' .
+                    '</div>' .  // report-author
+                    '<div class="report-date">' . $next_date . '</div>' .
+            '</div>' .
+            '<div class="report-text">' . $report['text'] . '</div>' .
+        '</div>';   // report
+
+        // $report_block = $report_block . '<div class="message_time ' . $message_align_css_class . '"><p>' . $message_time . '</p></div>';
+        
+        $report_blocks = $report_blocks . $next_report;
+    }
+    echo $report_blocks;    // форма с текстареа и кнопкой отправить
+
+    $img_file = '';
+    foreach ($files as $file) {
+        if (strpos($file, $user_from['login']) !== false) {
+            $img_file = $file;
+        }
+    }
+    if ($img_file == '') {
+        $img_file = $placeholder_image_name;
+    }
+    $img_route = $appserv_route_dir . '/' . $img_file;
+
+    echo '<div class="add-report">
+            <div class="send_message_form">
+                <form method="POST" action="">' .
+                    '<textarea name="report" class="report-text" title="Ваш комментарий" placeholder="Оставьте комментарий о профиле"></textarea><br>' .
+                    '<input type="submit" name="sendReport" class="submit-button" value="Отправить">' .
+                    //profile_link($user_from['id'], '<img src="' . $img_route . '" class="message_avatar send_message" title="' . $user_from['F'] . ' ' . $user_from['I'] . ' ' . $user_from['O'] . '"/>') .
+                '</form>
+            </div>
+        </div>';
+    echo '</div>';  // конец container
+}
+
+echo "<script>
+        function toggleReports() {
+            const reviews = document.getElementById('reports');
+            reviews.style.display = reviews.style.display === 'block' ? 'none' : 'block';
+        }
+    </script>";
 ?>
